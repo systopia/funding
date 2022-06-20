8661bc15b2eb97986b8e3b0be15e7fe6068c7243
Dominic Tubach <tubach@systopia.de>
2022-06-20 15:40:10 +0200

    RemoteFundingActionContactIdRequiredTrait: Initialize $remoteContactId with NULL because it's directly accessed

--- Civi/Funding/Api4/Action/Traits/RemoteFundingActionContactIdRequiredTrait.php
+++ Civi/Funding/Api4/Action/Traits/RemoteFundingActionContactIdRequiredTrait.php
@@ -3,19 +3,23 @@ declare(strict_types = 1);
 
 namespace Civi\Funding\Api4\Action\Traits;
 
+use Civi\Funding\Api4\Action\RemoteFundingActionInterface;
+
 trait RemoteFundingActionContactIdRequiredTrait {
 
   /**
-   * @var string
+   * Must be initialized because it is directly accessed in AbstractAction.
+   *
+   * @var string|null
    * @required
    */
-  protected string $remoteContactId;
+  protected ?string $remoteContactId = NULL;
 
-  public function getRemoteContactId(): string {
+  public function getRemoteContactId(): ?string {
     return $this->remoteContactId;
   }
 
-  public function setRemoteContactId(string $remoteContactId): self {
+  public function setRemoteContactId(string $remoteContactId): RemoteFundingActionInterface {
     $this->remoteContactId = $remoteContactId;
 
     return $this;
