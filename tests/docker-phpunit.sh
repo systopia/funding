9f4b2ea76d48ed4b6fff69bbc200ef09c7a5d594
Dominic Tubach <tubach@systopia.de>
2022-07-12 17:02:23 +0200

    docker-phpunit.sh: Workaround for Docker container with CiviCRM 5.51.0

--- tests/docker-phpunit.sh
+++ tests/docker-phpunit.sh
@@ -9,4 +9,10 @@ if [ ! -e tools/phpunit/vendor/bin ]; then
   "$SCRIPT_DIR/docker-prepare.sh"
 fi
 
-XDEBUG_MODE=coverage composer phpunit -- --cache-result-file=/tmp/.phpunit.result.cache "$@"
+export XDEBUG_MODE=coverage
+# TODO: Remove retry when not needed, anymore.
+# In Docker container with CiviCRM 5.51.0 the first run of phpunit fails with
+# the error below. For this reason, phpunit is run a second time, when the first
+# run fails.
+# Uncaught Error: Class 'Civi\Api4\SearchSegment' not found in /var/www/html/sites/all/modules/civicrm/ext/search_kit/Civi/Api4/Service/Spec/Provider/SearchSegmentExtraFieldProvider.php:52
+composer phpunit -- --cache-result-file=/tmp/.phpunit.result.cache "$@" || composer phpunit -- --cache-result-file=/tmp/.phpunit.result.cache "$@"
