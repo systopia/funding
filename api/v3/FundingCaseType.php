53015e2a0b9ead7b8b214c4a4d719ee6d1ee334f
Jens Schuppe <schuppe@systopia.de>
2022-09-01 12:42:01 +0200

    Add FundingCaseType.getlist API3 implementation for API4 autocomplete fields referencing funding case types

---
+++ api/v3/FundingCaseType.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Copyright (C) 2022 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+use Webmozart\Assert\Assert;
+
+/**
+ * Required for create/update in APIv4 explorer when FundingCaseType is
+ * referenced.
+ */
+
+/**
+ * @param array<string, mixed> $request
+ *
+ * @see _civicrm_api3_generic_getlist_params
+ */
+function _civicrm_api3_funding_case_type_getlist_params(array &$request): void {
+  _civicrm_api3_generic_getlist_params($request);
+}
+
+/**
+ * @param array<string, mixed> $result
+ * @param array<string, mixed> $request
+ *
+ * @return array<int, array<string, mixed>>
+ *
+ * @see _civicrm_api3_generic_getlist_output
+ */
+function _civicrm_api3_funding_case_type_getlist_output(array $result, array $request): array {
+  return _civicrm_api3_generic_getlist_output($result, $request, 'FundingCaseType', []);
+}
+
+/**
+ * @param array<string, mixed> $params
+ *
+ * @return array<string, mixed>
+ *
+ * @see _civicrm_api3_basic_get
+ */
+function civicrm_api3_funding_case_type_get(array $params): array {
+  $bao = _civicrm_api3_get_BAO(__FUNCTION__);
+  Assert::notNull($bao);
+
+  return _civicrm_api3_basic_get($bao, $params, TRUE, 'FundingCaseType');
+}
