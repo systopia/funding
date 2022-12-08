a81b5ccacc5f9e13c898963010617f16166035ae
Dominic Tubach <tubach@systopia.de>
2022-12-08 17:48:07 +0100

    Changes suggested in review

--- managed/OptionValueActivityUsedForApplicationProcess.mgd.php
+++ managed/OptionValueActivityUsedForApplicationProcess.mgd.php
@@ -2,6 +2,8 @@
 
 declare(strict_types = 1);
 
+use CRM_Funding_ExtensionUtil as E;
+
 return [
   [
     'name' => 'OptionValue_civicrm_funding_application_process',
@@ -12,22 +14,16 @@ return [
       'version' => 4,
       'values' => [
         'option_group_id.name' => 'activity_used_for',
-        'label' => 'civicrm_funding_application_process',
+        'label' => E::ts('Funding application process'),
         'value' => 'civicrm_funding_application_process',
-        'name' => 'civicrm_funding_application_process',
+        'name' => 'FundingApplicationProcess',
         'grouping' => 'funding',
         'filter' => 0,
         'is_default' => FALSE,
         'weight' => 1,
-        'description' => NULL,
         'is_optgroup' => FALSE,
         'is_reserved' => TRUE,
         'is_active' => TRUE,
-        'component_id' => NULL,
-        'domain_id' => NULL,
-        'visibility_id' => NULL,
-        'icon' => NULL,
-        'color' => NULL,
       ],
     ],
   ],
