4d0489b6432996a9e4df830fd9e19ed910736eec
Dominic Tubach <tubach@systopia.de>
2023-01-04 12:28:55 +0100

    Fix TestValidatedData

--- tests/phpunit/Civi/Funding/Mock/Form/FundingCaseType/TestValidatedData.php
+++ tests/phpunit/Civi/Funding/Mock/Form/FundingCaseType/TestValidatedData.php
@@ -30,6 +30,7 @@ use Civi\Funding\Form\ValidatedApplicationDataInterface;
  *   startDate: string,
  *   endDate: string,
  *   amountRequested: float,
+ *   comment?: string,
  * }
  */
 final class TestValidatedData implements ValidatedApplicationDataInterface {
@@ -73,6 +74,10 @@ final class TestValidatedData implements ValidatedApplicationDataInterface {
 
   public function getAmountRequested(): float {
     return $this->data['amountRequested'];
+  }
+
+  public function getComment(): ?string {
+    return $this->data['comment'] ?? NULL;
   }
 
   public function getApplicationData(): array {
