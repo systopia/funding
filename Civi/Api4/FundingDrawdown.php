39923a5e8a3e1887edc66abd66f143bdf4373d33
Dominic Tubach <tubach@systopia.de>
2023-04-17 13:34:38 +0200

    Add API to accept drawdowns

--- Civi/Api4/FundingDrawdown.php
+++ Civi/Api4/FundingDrawdown.php
@@ -3,7 +3,11 @@ declare(strict_types = 1);
 
 namespace Civi\Api4;
 
+use Civi\Funding\Api4\Action\FundingDrawdown\AcceptAction;
+use Civi\Funding\Api4\Action\FundingDrawdown\CreateAction;
 use Civi\Funding\Api4\Action\FundingDrawdown\GetAction;
+use Civi\Funding\Api4\Action\FundingDrawdown\SaveAction;
+use Civi\Funding\Api4\Action\FundingDrawdown\UpdateAction;
 use Civi\Funding\Api4\Traits\AccessPermissionsTrait;
 use Civi\RemoteTools\Api4\Traits\EntityNameTrait;
 
@@ -20,8 +24,24 @@ final class FundingDrawdown extends Generic\DAOEntity {
 
   use EntityNameTrait;
 
+  public static function accept(bool $checkPermissions = TRUE): AcceptAction {
+    return \Civi::service(AcceptAction::class)->setCheckPermissions($checkPermissions);
+  }
+
+  public static function create($checkPermissions = TRUE) {
+    return \Civi::service(CreateAction::class)->setCheckPermissions($checkPermissions);
+  }
+
   public static function get($checkPermissions = TRUE) {
     return \Civi::service(GetAction::class)->setCheckPermissions($checkPermissions);
+  }
+
+  public static function save($checkPermissions = TRUE) {
+    return \Civi::service(SaveAction::class)->setCheckPermissions($checkPermissions);
+  }
+
+  public static function update($checkPermissions = TRUE) {
+    return \Civi::service(UpdateAction::class)->setCheckPermissions($checkPermissions);
   }
 
 }
---
+++ Civi/Funding/PayoutProcess/DrawdownManager.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * Copyright (C) 2023 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Funding\PayoutProcess;
+
+use Civi\Api4\FundingDrawdown;
+use Civi\Core\CiviEventDispatcherInterface;
+use Civi\Funding\Entity\DrawdownEntity;
+use Civi\Funding\Event\PayoutProcess\DrawdownAcceptedEvent;
+use Civi\RemoteTools\Api4\Api4Interface;
+use Civi\RemoteTools\Api4\Query\Comparison;
+
+class DrawdownManager {
+
+  private Api4Interface $api4;
+
+  private CiviEventDispatcherInterface $eventDispatcher;
+
+  public function __construct(Api4Interface $api4, CiviEventDispatcherInterface $eventDispatcher) {
+    $this->api4 = $api4;
+    $this->eventDispatcher = $eventDispatcher;
+  }
+
+  public function accept(DrawdownEntity $drawdown, int $contactId): void {
+    $drawdown
+      ->setReviewerContactId($contactId)
+      ->setStatus('accepted')
+      ->setAcceptionDate(new \DateTime(date('Y-m-d H:i:s')));
+
+    $this->api4->updateEntity(
+      FundingDrawdown::_getEntityName(),
+      $drawdown->getId(),
+      $drawdown->toArray(),
+      ['checkPermissions' => FALSE],
+    );
+
+    $event = new DrawdownAcceptedEvent($drawdown);
+    $this->eventDispatcher->dispatch(DrawdownAcceptedEvent::class, $event);
+  }
+
+  public function get(int $id): ?DrawdownEntity {
+    $result = $this->api4->getEntities(
+      FundingDrawdown::_getEntityName(),
+      Comparison::new('id', '=', $id),
+      [],
+      1,
+      0,
+      ['checkPermissions' => FALSE],
+    );
+
+    return DrawdownEntity::singleOrNullFromApiResult($result);
+  }
+
+}
