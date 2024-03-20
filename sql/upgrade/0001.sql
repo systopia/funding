9bcc68bd38d6de3cebf423f71ee376bb14401a5d
Charlotte Jana <jana@systopia.de>
2024-03-20 11:18:33 +0100

    fix error in upgrade sql file

--- sql/upgrade/0001.sql
+++ sql/upgrade/0001.sql
@@ -7,7 +7,7 @@ ALTER TABLE civicrm_funding_app_cost_item
 UPDATE civicrm_funding_app_cost_item SET identifier = REPLACE(identifier, '/', '.');
 
 ALTER TABLE civicrm_funding_application_snapshot
-  ADD resources_item text NOT NULL AFTER cost_items;
+  ADD resources_items text NOT NULL AFTER cost_items;
 
 ALTER TABLE civicrm_funding_app_resources_item
   ADD data_pointer varchar(255) NOT NULL COMMENT 'JSON pointer to data in application data';
