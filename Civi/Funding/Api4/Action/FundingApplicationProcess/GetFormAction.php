cba25518fafb56c80fb21f850d5e30e6b22d4a58
Dominic Tubach <tubach@systopia.de>
2024-07-09 17:14:42 +0200

    Allow to use JSON Forms for application review forms
    The AngularJS scope now contains the UI schema, too.

---
+++ Civi/Funding/Api4/Action/FundingApplicationProcess/GetFormAction.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright (C) 2024 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Funding\Api4\Action\FundingApplicationProcess;
+
+use Civi\Api4\FundingApplicationProcess;
+use Civi\Api4\Generic\AbstractAction;
+use Civi\RemoteTools\ActionHandler\ActionHandlerInterface;
+use Civi\RemoteTools\Api4\Action\Traits\ActionHandlerRunTrait;
+use Civi\RemoteTools\Api4\Action\Traits\IdParameterTrait;
+
+class GetFormAction extends AbstractAction {
+
+  use ActionHandlerRunTrait;
+
+  use IdParameterTrait;
+
+  public function __construct(ActionHandlerInterface $actionHandler = NULL) {
+    parent::__construct(FundingApplicationProcess::getEntityName(), 'getForm');
+    $this->setActionHandler($actionHandler);
+  }
+
+}
---
+++ Civi/Funding/ApplicationProcess/Api4/ActionHandler/GetFormActionHandler.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Copyright (C) 2024 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Funding\ApplicationProcess\Api4\ActionHandler;
+
+use Civi\Funding\Api4\Action\FundingApplicationProcess\GetFormAction;
+use Civi\Funding\ApplicationProcess\ApplicationProcessBundleLoader;
+use Civi\Funding\ApplicationProcess\Command\ApplicationFormCreateCommand;
+use Civi\Funding\ApplicationProcess\Handler\ApplicationFormCreateHandlerInterface;
+use Civi\RemoteTools\ActionHandler\ActionHandlerInterface;
+use Webmozart\Assert\Assert;
+
+/**
+ * @phpstan-type formResultT array{
+ *   jsonSchema: array<int|string, mixed>,
+ *   uiSchema: array<int|string, mixed>,
+ *   data: array<string, mixed>,
+ * }
+ */
+final class GetFormActionHandler implements ActionHandlerInterface {
+
+  public const ENTITY_NAME = 'FundingApplicationProcess';
+
+  private ApplicationProcessBundleLoader $applicationProcessBundleLoader;
+
+  private ApplicationFormCreateHandlerInterface $createHandler;
+
+  public function __construct(
+    ApplicationProcessBundleLoader $applicationProcessBundleLoader,
+    ApplicationFormCreateHandlerInterface $createHandler
+  ) {
+    $this->applicationProcessBundleLoader = $applicationProcessBundleLoader;
+    $this->createHandler = $createHandler;
+  }
+
+  /**
+   * @phpstan-return formResultT
+   *
+   * @throws \CRM_Core_Exception
+   */
+  public function getForm(GetFormAction $action): array {
+    $applicationProcessBundle = $this->applicationProcessBundleLoader->get($action->getId());
+    Assert::notNull($applicationProcessBundle, sprintf('Application process with ID %d not found', $action->getId()));
+    $applicationProcessStatusList = $this->applicationProcessBundleLoader->getStatusList($applicationProcessBundle);
+
+    $form = $this->createHandler->handle(new ApplicationFormCreateCommand(
+      $applicationProcessBundle,
+      $applicationProcessStatusList)
+    );
+
+    return [
+      'jsonSchema' => $form->getJsonSchema()->toArray(),
+      'uiSchema' => $form->getUiSchema()->toArray(),
+      'data' => $form->getData(),
+    ];
+  }
+
+}
