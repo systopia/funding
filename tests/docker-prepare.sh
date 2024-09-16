b437d56b6ffbbadc391c6be0cf65b27e955902a7
Dominic Tubach <tubach@systopia.de>
2024-09-16 12:17:07 +0200

    chore: Update phpstan and phpunit related files

--- tests/docker-prepare.sh
+++ tests/docker-prepare.sh
@@ -47,7 +47,13 @@ else
   # The autoloader expected class "Civi\ActionSchedule\Mapping" to be defined in
   # file "[...]/Civi/ActionSchedule/Mapping.php". The file was found but the
   # class was not in it, the class name or namespace probably has a typo.
-  rm -f /var/www/html/sites/all/modules/civicrm/Civi/ActionSchedule/Mapping.php
+  #
+  # Necessary for CiviCRM 5.66.0 - 5.74.x.
+  # https://github.com/civicrm/civicrm-core/blob/5.66.0/Civi/ActionSchedule/Mapping.php
+  if [ -e /var/www/html/sites/all/modules/civicrm/Civi/ActionSchedule/Mapping.php ] \
+      && grep -q '// Empty file' /var/www/html/sites/all/modules/civicrm/Civi/ActionSchedule/Mapping.php; then
+    rm /var/www/html/sites/all/modules/civicrm/Civi/ActionSchedule/Mapping.php
+  fi
 
   # For headless tests these files need to exist.
   touch /var/www/html/sites/all/modules/civicrm/sql/test_data.mysql
