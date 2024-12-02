b01bd274851d6a5b57b1409c8cd82a64bc864052
Dominic Tubach <tubach@systopia.de>
2024-12-02 18:07:00 +0100

    Add drawdown task, refactor

---
+++ Civi/Funding/Event/PayoutProcess/DrawdownUpdatedEvent.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Copyright (C) 2024 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Funding\Event\PayoutProcess;
+
+use Civi\Funding\Entity\DrawdownEntity;
+use Civi\Funding\Entity\DrawdownBundle;
+use Civi\Funding\Entity\Traits\DrawdownBundleTrait;
+
+final class DrawdownUpdatedEvent {
+
+  use DrawdownBundleTrait;
+
+  private DrawdownEntity $previousDrawdown;
+
+  public function __construct(
+    DrawdownEntity $previousDrawdown,
+    DrawdownBundle $drawdownBundle
+  ) {
+    $this->previousDrawdown = $previousDrawdown;
+    $this->drawdownBundle = $drawdownBundle;
+  }
+
+  public function getPreviousDrawdown(): DrawdownEntity {
+    return $this->previousDrawdown;
+  }
+
+}
