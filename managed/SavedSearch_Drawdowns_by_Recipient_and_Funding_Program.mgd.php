efa42baa707cda3d6bcdb56ea5f24f75eed44a42
Dominic Tubach <tubach@systopia.de>
2024-12-09 10:53:29 +0100

    Fix calculation of amount approved in drawdown controlling
    Previously the amount approved of one funding case was used multiple
    times. Once for each drawdown.

--- managed/SavedSearch_Drawdowns_by_Recipient_and_Funding_Program.mgd.php
+++ managed/SavedSearch_Drawdowns_by_Recipient_and_Funding_Program.mgd.php
@@ -1,9 +1,6 @@
 <?php
-declare(strict_types = 1);
-
 use CRM_Funding_ExtensionUtil as E;
 
-// phpcs:disable Generic.Files.LineLength.TooLong
 return [
   [
     'name' => 'SavedSearch_Drawdowns_by_Recipient_and_Funding_Program',
@@ -15,51 +12,58 @@ return [
       'values' => [
         'name' => 'Drawdowns_by_Recipient_and_Funding_Program',
         'label' => E::ts('Drawdowns by Recipient and Funding Program'),
-        'api_entity' => 'FundingDrawdown',
+        'api_entity' => 'FundingCase',
         'api_params' => [
           'version' => 4,
           'select' => [
             'COUNT(id) AS COUNT_id',
-            'GROUP_CONCAT(DISTINCT FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_FundingCase_FundingProgram_funding_program_id_01.title) AS GROUP_CONCAT_FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_FundingCase_FundingProgram_funding_program_id_01_title',
-            'GROUP_CONCAT(DISTINCT FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.recipient_contact_id.display_name) AS GROUP_CONCAT_FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_recipient_contact_id_display_name',
-            'SUM(FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.amount_approved) AS SUM_FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_amount_approved',
-            'SUM(amount) AS SUM_amount',
-            'SUM(amount_paid_out) AS SUM_amount_paid_out',
-            'SUM(amount_new) AS SUM_amount_new',
-            'SUM(creation_date) AS SUM_creation_date',
+            'GROUP_CONCAT(DISTINCT FundingCase_FundingProgram_funding_program_id_01.title) AS GROUP_CONCAT_FundingCase_FundingProgram_funding_program_id_01_title',
+            'GROUP_CONCAT(DISTINCT recipient_contact_id.display_name) AS GROUP_CONCAT_recipient_contact_id_display_name',
+            'SUM(amount_approved) AS SUM_amount_approved',
+            'SUM(FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01.amount) AS SUM_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_amount',
+            'SUM(FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01.amount_paid_out) AS SUM_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_amount_paid_out',
+            'SUM(FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01.amount_new) AS SUM_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_amount_new',
+            'GROUP_FIRST(FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01.creation_date) AS GROUP_FIRST_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_creation_date',
+            'GROUP_FIRST(FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01.acception_date) AS GROUP_FIRST_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_acception_date',
           ],
           'orderBy' => [],
-          'where' => [],
+          'where' => [
+            [
+              'amount_approved',
+              '>=',
+              '0',
+            ],
+          ],
           'groupBy' => [
-            'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.recipient_contact_id',
-            'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.funding_program_id',
+            'funding_program_id',
+            'recipient_contact_id',
           ],
           'join' => [
             [
-              'FundingPayoutProcess AS FundingDrawdown_FundingPayoutProcess_payout_process_id_01',
-              'LEFT',
+              'FundingProgram AS FundingCase_FundingProgram_funding_program_id_01',
+              'INNER',
               [
-                'payout_process_id',
+                'funding_program_id',
                 '=',
-                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01.id',
+                'FundingCase_FundingProgram_funding_program_id_01.id',
               ],
             ],
             [
-              'FundingCase AS FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01',
+              'FundingPayoutProcess AS FundingCase_FundingPayoutProcess_funding_case_id_01',
               'LEFT',
               [
-                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01.funding_case_id',
+                'id',
                 '=',
-                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.id',
+                'FundingCase_FundingPayoutProcess_funding_case_id_01.funding_case_id',
               ],
             ],
             [
-              'FundingProgram AS FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_FundingCase_FundingProgram_funding_program_id_01',
+              'FundingDrawdown AS FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01',
               'LEFT',
               [
-                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01.funding_program_id',
+                'FundingCase_FundingPayoutProcess_funding_case_id_01.id',
                 '=',
-                'FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_FundingCase_FundingProgram_funding_program_id_01.id',
+                'FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01.payout_process_id',
               ],
             ],
           ],
@@ -72,7 +76,7 @@ return [
     ],
   ],
   [
-    'name' => 'SavedSearch_Drawdowns_by_Recipient_and_Funding_Program_SearchDisplay_Drawdowns_by_Recipient_and_Funding_Program_Table',
+    'name' => 'SavedSearch_Drawdowns_by_Recipient_and_Funding_Program_SearchDisplay_Table',
     'entity' => 'SearchDisplay',
     'cleanup' => 'unused',
     'update' => 'unmodified',
@@ -92,27 +96,21 @@ return [
           'columns' => [
             [
               'type' => 'field',
-              'key' => 'GROUP_CONCAT_FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_FundingCase_FundingProgram_funding_program_id_01_title',
+              'key' => 'GROUP_CONCAT_FundingCase_FundingProgram_funding_program_id_01_title',
               'dataType' => 'String',
               'label' => E::ts('Funding Program'),
               'sortable' => TRUE,
-              'tally' => [
-                'fn' => NULL,
-              ],
             ],
             [
               'type' => 'field',
-              'key' => 'GROUP_CONCAT_FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_recipient_contact_id_display_name',
+              'key' => 'GROUP_CONCAT_recipient_contact_id_display_name',
               'dataType' => 'String',
               'label' => E::ts('Recipient'),
               'sortable' => TRUE,
-              'tally' => [
-                'fn' => NULL,
-              ],
             ],
             [
               'type' => 'field',
-              'key' => 'SUM_FundingDrawdown_FundingPayoutProcess_payout_process_id_01_FundingPayoutProcess_FundingCase_funding_case_id_01_amount_approved',
+              'key' => 'SUM_amount_approved',
               'dataType' => 'Money',
               'label' => E::ts('Amount Approved'),
               'sortable' => TRUE,
@@ -122,7 +120,7 @@ return [
             ],
             [
               'type' => 'field',
-              'key' => 'SUM_amount',
+              'key' => 'SUM_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_amount',
               'dataType' => 'Money',
               'label' => E::ts('Drawdowns'),
               'sortable' => TRUE,
@@ -132,7 +130,7 @@ return [
             ],
             [
               'type' => 'field',
-              'key' => 'SUM_amount_paid_out',
+              'key' => 'SUM_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_amount_paid_out',
               'dataType' => 'Money',
               'label' => E::ts('Amount Paid Out'),
               'sortable' => TRUE,
@@ -142,7 +140,7 @@ return [
             ],
             [
               'type' => 'field',
-              'key' => 'SUM_amount_new',
+              'key' => 'SUM_FundingCase_FundingPayoutProcess_funding_case_id_01_FundingPayoutProcess_FundingDrawdown_payout_process_id_01_amount_new',
               'dataType' => 'Money',
               'label' => E::ts('Open Drawdowns'),
               'sortable' => TRUE,
