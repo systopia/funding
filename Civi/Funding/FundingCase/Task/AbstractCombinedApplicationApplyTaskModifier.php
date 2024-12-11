e07e515751f4a1ee338911811b269edeb4dd862c
Dominic Tubach <tubach@systopia.de>
2024-12-11 16:39:07 +0100

    Add apply task for combined applications

---
+++ Civi/Funding/FundingCase/Task/AbstractCombinedApplicationApplyTaskModifier.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * Copyright (C) 2024 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Funding\FundingCase\Task;
+
+use Civi\Funding\ActivityStatusNames;
+use Civi\Funding\ApplicationProcess\ApplicationProcessManager;
+use Civi\Funding\Entity\FundingCaseBundle;
+use Civi\Funding\Entity\FundingCaseEntity;
+use Civi\Funding\Entity\FundingTaskEntity;
+use Civi\Funding\FundingCase\Traits\CombinedApplicationCaseTaskTrait;
+use Civi\Funding\Task\Modifier\FundingCaseTaskModifierInterface;
+use Civi\RemoteTools\Api4\Query\CompositeCondition;
+
+/**
+ * Completes an apply funding case task if there's no remaining application
+ * process that is in a status in which it can be applied.
+ *
+ * Should be combined with:
+ * @see \Civi\Funding\FundingCase\Task\AbstractCombinedApplicationApplyTaskCreator
+ */
+abstract class AbstractCombinedApplicationApplyTaskModifier implements FundingCaseTaskModifierInterface {
+
+  use CombinedApplicationCaseTaskTrait;
+
+  private ApplicationProcessManager $applicationProcessManager;
+
+  public function __construct(ApplicationProcessManager $applicationProcessManager) {
+    $this->applicationProcessManager = $applicationProcessManager;
+  }
+
+  public function modifyTask(
+    FundingTaskEntity $task,
+    FundingCaseBundle $fundingCaseBundle,
+    FundingCaseEntity $previousFundingCase
+  ): bool {
+    if (self::$taskType !== $task->getType() || $this->isInAppliableStatus($fundingCaseBundle)) {
+      return FALSE;
+    }
+
+    $task->setStatusName(ActivityStatusNames::COMPLETED);
+
+    return TRUE;
+  }
+
+  private function isInAppliableStatus(FundingCaseBundle $fundingCaseBundle): bool {
+    return 0 !== $this->applicationProcessManager->countBy(CompositeCondition::fromFieldValuePairs([
+      'funding_case_id' => $fundingCaseBundle->getFundingCase()->getId(),
+      'status' => $this->getAppliableStatusList(),
+    ]));
+  }
+
+}
---
+++ tests/phpunit/Civi/Funding/FundingCase/Task/AbstractCombinedApplicationApplyTaskCreatorTest.php
@@ -0,0 +1,109 @@
+<?php
+declare(strict_types = 1);
+
+namespace Civi\Funding\FundingCase\Task;
+
+use Civi\Funding\Entity\FundingTaskEntity;
+use Civi\Funding\EntityFactory\ApplicationProcessBundleFactory;
+use Civi\Funding\EntityFactory\ApplicationProcessFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Civi\Funding\FundingCase\Task\AbstractCombinedApplicationApplyTaskCreator
+ */
+final class AbstractCombinedApplicationApplyTaskCreatorTest extends TestCase {
+
+  private AbstractCombinedApplicationApplyTaskCreator $taskCreator;
+
+  protected function setUp(): void {
+    parent::setUp();
+    $this->taskCreator = $this->getMockForAbstractClass(AbstractCombinedApplicationApplyTaskCreator::class);
+  }
+
+  /**
+   * @dataProvider provideAppliableStatus
+   */
+  public function testCreateTasksOnChangeStatusAppliable(string $appliableStatus): void {
+    $applicationProcessBundle = ApplicationProcessBundleFactory::createApplicationProcessBundle([
+      'status' => $appliableStatus,
+    ]);
+    $previousApplicationProcess = ApplicationProcessFactory::createApplicationProcess(['status' => 'test']);
+
+    static::assertEquals([
+      FundingTaskEntity::newTask([
+        'subject' => 'Complete and apply application',
+        'affected_identifier' => $applicationProcessBundle->getFundingCase()->getIdentifier(),
+        'required_permissions' => ['application_apply'],
+        'type' => 'apply',
+        'funding_case_id' => $applicationProcessBundle->getFundingCase()->getId(),
+      ]),
+    ], [...$this->taskCreator->createTasksOnChange($applicationProcessBundle, $previousApplicationProcess)]);
+  }
+
+  public function testCreateTasksOnChangeStatusUnchanged(): void {
+    $applicationProcessBundle = ApplicationProcessBundleFactory::createApplicationProcessBundle([
+      'status' => 'draft',
+      'short_description' => 'foo',
+    ]);
+    $previousApplicationProcess = ApplicationProcessFactory::createApplicationProcess([
+      'status' => 'draft',
+      'short_description' => 'bar',
+    ]);
+
+    static::assertSame(
+      [],
+      [...$this->taskCreator->createTasksOnChange($applicationProcessBundle, $previousApplicationProcess)]
+    );
+  }
+
+  public function testCreateTasksOnChangeStatusNotAppliable(): void {
+    $applicationProcessBundle = ApplicationProcessBundleFactory::createApplicationProcessBundle([
+      'status' => 'applied',
+    ]);
+    $previousApplicationProcess = ApplicationProcessFactory::createApplicationProcess([
+      'status' => 'draft',
+    ]);
+
+    static::assertSame(
+      [],
+      [...$this->taskCreator->createTasksOnChange($applicationProcessBundle, $previousApplicationProcess)]
+    );
+  }
+
+  /**
+   * @dataProvider provideAppliableStatus
+   */
+  public function testCreateTasksOnNewAppliable(string $appliableStatus): void {
+    $applicationProcessBundle = ApplicationProcessBundleFactory::createApplicationProcessBundle([
+      'status' => $appliableStatus,
+    ]);
+
+    static::assertEquals([
+      FundingTaskEntity::newTask([
+        'subject' => 'Complete and apply application',
+        'affected_identifier' => $applicationProcessBundle->getFundingCase()->getIdentifier(),
+        'required_permissions' => ['application_apply'],
+        'type' => 'apply',
+        'funding_case_id' => $applicationProcessBundle->getFundingCase()->getId(),
+      ]),
+    ], [...$this->taskCreator->createTasksOnNew($applicationProcessBundle)]);
+  }
+
+  public function testCreateTasksOnNewNotAppliable(): void {
+    $applicationProcessBundle = ApplicationProcessBundleFactory::createApplicationProcessBundle([
+      'status' => 'applied',
+    ]);
+
+    static::assertSame([], [...$this->taskCreator->createTasksOnNew($applicationProcessBundle)]);
+  }
+
+  /**
+   * @phpstan-return iterable<array{string}>
+   */
+  public function provideAppliableStatus(): iterable {
+    yield ['new'];
+    yield ['draft'];
+    yield ['rework'];
+  }
+
+}
