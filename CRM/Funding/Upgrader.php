c802c2c84b86039981a8774daef2be2d96df228e
Tobias Bähr <tobiasbaehr@users.noreply.github.com>
2025-05-21 15:47:59 +0200

    fix(sql): Can not apply upgrade_0011
    * 'JSON_OVERLAPS(NULL, NULL)' is not a valid sql-statement.
    
    * Do not use 'CREATE OR REPLACE FUNCTION', but run 'DROP FUNCTION IF EXISTS' which works also in mysql.

--- CRM/Funding/Upgrader.php
+++ CRM/Funding/Upgrader.php
@@ -143,8 +143,9 @@ final class CRM_Funding_Upgrader extends CRM_Extension_Upgrader_Base {
   }
 
   private function installJsonOverlapsSqlFunction(): void {
+    CRM_Core_DAO::executeQuery('DROP FUNCTION IF EXISTS FUNDING_JSON_OVERLAPS');
     try {
-      CRM_Core_DAO::executeQuery('JSON_OVERLAPS(NULL, NULL)');
+      CRM_Core_DAO::executeQuery('SELECT JSON_OVERLAPS(NULL, NULL)');
       // Native JSON_OVERLAPS exists (MariaDB >=10.9)
       $this->executeFullSqlFile('sql/functions/funding_json_overlaps_alias.sql');
     }
