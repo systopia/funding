15667067de2df2a7756c1e350037a23197524290
Dominic Tubach <tubach@systopia.de>
2025-06-26 09:47:17 +0200

    HiH: Add receipts form for clearing

--- Civi/Funding/Entity/ClearingCostItemEntity.php
+++ Civi/Funding/Entity/ClearingCostItemEntity.php
@@ -28,11 +28,13 @@ namespace Civi\Funding\Entity;
  *   file_id: ?int,
  *   receipt_number: ?string,
  *   receipt_date: ?string,
- *   payment_date: string,
- *   recipient: string,
- *   reason: string,
+ *   payment_date: ?string,
+ *   recipient: ?string,
+ *   reason: ?string,
  *   amount: float,
  *   amount_admitted: ?float,
+ *   properties: ?array<string, mixed>,
+ *   form_key: string,
  * }
  *
  * @phpstan-extends AbstractClearingItemEntity<clearingCostItemT>
