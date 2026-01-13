68344bb327df8658b21240b783b069eac774fabc
Dominic Tubach <tubach@systopia.de>
2026-01-13 17:38:32 +0100

    SearchKit tasks: Adapt to changes in CiviCRM 6.10
    This adapts to changes in CiviCRM 6.10 introduced by
    https://github.com/civicrm/civicrm-core/pull/33912.

--- ang/crmFunding/drawdown/drawdownSearchTasksMenu.decorator.js
+++ ang/crmFunding/drawdown/drawdownSearchTasksMenu.decorator.js
@@ -56,136 +56,136 @@ fundingModule.directive('fundingDrawdownTasksDecorator', function() {
 
         // See TaskManager in searchDisplayTasksTrait
         const taskManager = ctrl.taskManager;
-        const entityName = taskManager.getEntityName();
 
-        if (entityName !== 'FundingDrawdown') {
-          return;
-        }
-
-        const ts = CRM.ts('funding');
-
-        let allowedActionsByDrawdown = {};
-
-        let searchKitTasks;
         taskManager.getMetadata().then(() => {
-          taskManager.entityInfo = taskManager.entityInfo || {};
+          const entityName = taskManager.entityInfo.name;
+          if (entityName !== 'FundingDrawdown') {
+            return;
+          }
+
+          const ts = CRM.ts('funding');
+
+          let allowedActionsByDrawdown = {};
+
           taskManager.entityInfo.title = ts('Drawdown');
           taskManager.entityInfo.title_plural = ts('Drawdowns');
-          searchKitTasks = taskManager.tasks;
-        });
+          const searchKitTasks = taskManager.tasks;
 
-        function updateAvailableTasks() {
-          if (ctrl.ids.length === 0) {
-            taskManager.tasks = searchKitTasks;
-            return;
-          }
-
-          let tasks = {};
-          const firstActions = allowedActionsByDrawdown[ctrl.ids[0]] || {};
-          for (const [actionName, {label, confirm}] of Object.entries(firstActions)) {
-            tasks[actionName] = {
-              name: actionName,
-              title: label,
-              confirm: confirm,
-              _customTask: true,
-            };
-          }
-
-          // Filter out tasks that are not available in all selected drawdowns
-          // have a different label.
-          for (let i = 1; i < ctrl.ids.length; ++i) {
-            const actions = allowedActionsByDrawdown[ctrl.ids[i]] || {};
-            tasks = _4.pickBy(tasks, (task, actionName) => actions[actionName] && actions[actionName].label === task.title);
-          }
-
-          taskManager.tasks = searchKitTasks.concat(Object.values(tasks));
-        }
-
-        let lastIds;
-        function updateTasks() {
-          if (_4.isEqual(lastIds, ctrl.ids)) {
-            return new Promise((resolve) => resolve([]));
-          }
-
-          lastIds = _4.clone(ctrl.ids);
-          const idsToGetActions = [];
-          ctrl.ids.forEach((id) => {
-            if (!allowedActionsByDrawdown[id]) {
-              idsToGetActions.push(id);
+          function updateAvailableTasks() {
+            if (ctrl.ids.length === 0) {
+              taskManager.tasks = searchKitTasks;
+              return;
             }
-          });
 
-          if (idsToGetActions.length > 0) {
-            taskManager.tasks = searchKitTasks;
-            crmApi4('FundingDrawdown', 'get', {
-              select: ['id', 'CAN_review'],
-              where: [['id', 'IN', idsToGetActions]]
-            }).then(function (drawdowns) {
-              for (const drawdown of drawdowns) {
-                if (drawdown.CAN_review) {
-                  allowedActionsByDrawdown[drawdown.id] = {
-                    accept: {label: ts('Accept'), confirm: ts('Do you want to accept the selected drawdowns?')},
-                    reject: {label: ts('Reject'), confirm: ts('Do you want to reject the selected drawdowns?')},
-                  };
-                }
-                else {
-                  allowedActionsByDrawdown[drawdown.id] = {};
-                }
+            let tasks = {};
+            const firstActions = allowedActionsByDrawdown[ctrl.ids[0]] || {};
+            for (const [actionName, {label, confirm}] of Object.entries(firstActions)) {
+              tasks[actionName] = {
+                name: actionName,
+                title: label,
+                confirm: confirm,
+                _customTask: true,
+              };
+            }
+
+            // Filter out tasks that are not available in all selected drawdowns
+            // have a different label.
+            for (let i = 1; i < ctrl.ids.length; ++i) {
+              const actions = allowedActionsByDrawdown[ctrl.ids[i]] || {};
+              tasks = _4.pickBy(tasks, (task, actionName) => actions[actionName] && actions[actionName].label === task.title);
+            }
+
+            taskManager.tasks = searchKitTasks.concat(Object.values(tasks));
+          }
+
+          let lastIds;
+
+          function updateTasks() {
+            if (_4.isEqual(lastIds, ctrl.ids)) {
+              return new Promise((resolve) => resolve([]));
+            }
+
+            lastIds = _4.clone(ctrl.ids);
+            const idsToGetActions = [];
+            ctrl.ids.forEach((id) => {
+              if (!allowedActionsByDrawdown[id]) {
+                idsToGetActions.push(id);
               }
-
-              updateAvailableTasks();
             });
-          } else {
-            updateAvailableTasks();
-            return new Promise((resolve) => resolve([]));
-          }
-        }
 
-        // Only triggered if a new ID is selected, not un deselect.
-        $scope.$watch('$ctrl.ids', () => updateTasks());
+            if (idsToGetActions.length > 0) {
+              taskManager.tasks = searchKitTasks;
+              crmApi4('FundingDrawdown', 'get', {
+                select: ['id', 'CAN_review'],
+                where: [['id', 'IN', idsToGetActions]]
+              }).then(function (drawdowns) {
+                for (const drawdown of drawdowns) {
+                  if (drawdown.CAN_review) {
+                    allowedActionsByDrawdown[drawdown.id] = {
+                      accept: {label: ts('Accept'), confirm: ts('Do you want to accept the selected drawdowns?')},
+                      reject: {label: ts('Reject'), confirm: ts('Do you want to reject the selected drawdowns?')},
+                    };
+                  }
+                  else {
+                    allowedActionsByDrawdown[drawdown.id] = {};
+                  }
+                }
 
-        taskManager.getMetadata = updateTasks;
-
-        const parentDoTask = taskManager.doTask;
-        taskManager.doTask = function(action, ids) {
-          if (!action._customTask) {
-            parentDoTask(action, ids);
-
-            return;
-          }
-
-          CRM.confirm({message: action.confirm}).on('crmConfirm:yes', function() {
-            crmStatus({}, crmApi4('FundingDrawdown', action.name + 'Multiple', {ids}).then(() => {
-              for (const id of ids) {
-                allowedActionsByDrawdown[id] = {};
-              }
-
-              if (action.name === 'accept') {
-                window.open(CRM.url('civicrm/funding/drawdown-document/download', {drawdownIds: ids.join(',')}));
-              }
-
-              taskManager.refreshAfterTask();
-
-              const event = new CustomEvent('drawdownSearchTaskExecuted', {
-                detail: {
-                  entity: entityName,
-                  ids: ids,
-                  action: action.name,
-                },
+                updateAvailableTasks();
               });
-              document.dispatchEvent(event);
-            }));
-          });
-        };
-
-        const parentIsActionAllowed = ctrl.isActionAllowed;
-        ctrl.isActionAllowed = function (action) {
-          if (action._customTask) {
-            return true;
+            }
+            else {
+              updateAvailableTasks();
+              return new Promise((resolve) => resolve([]));
+            }
           }
 
-          return parentIsActionAllowed(action);
-        };
+          // Only triggered if a new ID is selected, not un deselect.
+          $scope.$watch('$ctrl.ids', () => updateTasks());
+
+          taskManager.getMetadata = updateTasks;
+
+          const parentDoTask = taskManager.doTask;
+          taskManager.doTask = function (action, ids) {
+            if (!action._customTask) {
+              parentDoTask(action, ids);
+
+              return;
+            }
+
+            CRM.confirm({message: action.confirm}).on('crmConfirm:yes', function () {
+              crmStatus({}, crmApi4('FundingDrawdown', action.name + 'Multiple', {ids}).then(() => {
+                for (const id of ids) {
+                  allowedActionsByDrawdown[id] = {};
+                }
+
+                if (action.name === 'accept') {
+                  window.open(CRM.url('civicrm/funding/drawdown-document/download', {drawdownIds: ids.join(',')}));
+                }
+
+                taskManager.refreshAfterTask();
+
+                const event = new CustomEvent('drawdownSearchTaskExecuted', {
+                  detail: {
+                    entity: entityName,
+                    ids: ids,
+                    action: action.name,
+                  },
+                });
+                document.dispatchEvent(event);
+              }));
+            });
+          };
+
+          const parentIsActionAllowed = ctrl.isActionAllowed;
+          ctrl.isActionAllowed = function (action) {
+            if (action._customTask) {
+              return true;
+            }
+
+            return parentIsActionAllowed(action);
+          };
+        });
       }
     ],
   };
