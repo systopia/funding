498a85a7edb247c753c3cd2ffa20797031739eed
Dominic Tubach <tubach@systopia.de>
2026-02-25 10:29:33 +0100

    Remove license header from .sql file

--- sql/upgrade/0019.sql
+++ sql/upgrade/0019.sql
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2025 SYSTOPIA GmbH
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 CREATE TABLE `civicrm_funding_form_string_translation` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Unique FundingFormStringTranslation ID',
   `funding_program_id` int unsigned NOT NULL COMMENT 'FK to FundingProgram',
