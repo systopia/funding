332aac9713f9043ecddfafeb3443d65703a06375
Matthias Gutjahr <gutjahr@systopia.de>
2026-03-24 17:32:31 +0100

    add-adb-fundingcasetypes

---
+++ Civi/Funding/FundingCaseTypes/AdB/SonstigeAktivitaet/Application/UISchema/AVK1GrunddatenUiSchema.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * Copyright (C) 2025 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Funding\FundingCaseTypes\AdB\SonstigeAktivitaet\Application\UISchema;
+
+use Civi\RemoteTools\JsonForms\Control\JsonFormsArray;
+use Civi\RemoteTools\JsonForms\JsonFormsControl;
+use Civi\RemoteTools\JsonForms\JsonFormsMarkup;
+use Civi\RemoteTools\JsonForms\Layout\JsonFormsCategory;
+use Civi\RemoteTools\JsonForms\Layout\JsonFormsGroup;
+
+final class AVK1GrunddatenUiSchema extends JsonFormsCategory {
+
+  public function __construct(string $scopePrefix) {
+    $elements = [
+      new JsonFormsMarkup(<<<EOD
+<p>Bei Sonstigen Aktivitäten kann die Summe einer Kostenkategorie (Einzelansätze,
+nicht die einzelne Positionen) um bis zu 20 % überschritten werden, solange die
+Überschreitung durch entsprechende Einsparungen bei anderen Einzelansätzen
+ausgeglichen werden kann. Die bewilligte Gesamtsumme kann nicht überschritten
+werden.</p>
+EOD
+      ),
+      new JsonFormsControl(
+        "$scopePrefix/internerBezeichner",
+        'Interner Bezeichner'
+      ),
+      new JsonFormsControl(
+        "$scopePrefix/titel",
+        'Titel'
+      ),
+      new JsonFormsControl(
+        "$scopePrefix/kurzbeschreibungDesInhalts",
+        'Kurzbeschreibung des Inhalts',
+        NULL,
+        [
+          'multi' => TRUE,
+          'placeholder' => 'Maximal 500 Zeichen',
+        ]
+      ),
+      new JsonFormsArray(
+        "$scopePrefix/zeitraeume",
+        'Zeiträume',
+        NULL,
+        [
+          new JsonFormsControl('#/properties/beginn', 'Beginn'),
+          new JsonFormsControl('#/properties/ende', 'Ende'),
+        ],
+        [
+          'addButtonLabel' => 'Zeitraum hinzufügen',
+          'removeButtonLabel' => 'Zeitraum entfernen',
+        ]
+      ),
+      new JsonFormsGroup('Teilnehmer*innen', [
+        new JsonFormsControl(
+          "$scopePrefix/teilnehmer/properties/gesamt",
+          'Gesamtanzahl der Teilnehmer*innen'
+        ),
+        new JsonFormsControl(
+          "$scopePrefix/teilnehmer/properties/weiblich",
+          'davon weiblich'
+        ),
+        new JsonFormsControl(
+          "$scopePrefix/teilnehmer/properties/divers",
+          'davon divers'
+        ),
+        new JsonFormsControl(
+          "$scopePrefix/teilnehmer/properties/unter27",
+          'davon U27'
+        ),
+        new JsonFormsControl(
+          "$scopePrefix/teilnehmer/properties/inJugendhilfeEhrenamtlichTaetig",
+          'davon in der Kinder- und Jugendhilfe (Multiplikator*innen-Seminare) ehrenamtlich tätig',
+        ),
+        new JsonFormsControl(
+          "$scopePrefix/teilnehmer/properties/inJugendhilfeHauptamtlichTaetig",
+          'davon in der Kinder- und Jugendhilfe (Multiplikator*innen-Seminare) hauptamtlich tätig',
+        ),
+        new JsonFormsControl(
+          "$scopePrefix/teilnehmer/properties/referenten",
+          'davon Referent*innen'
+        ),
+      ], 'Wie viele Teilnehmer*innen werden für die Veranstaltung erwartet?'),
+    ];
+
+    parent::__construct(
+      'Grunddaten',
+      $elements,
+    );
+  }
+
+}
---
+++ Civi/Funding/FundingCaseTypes/AdB/SonstigeAktivitaet/Report/JsonSchema/AVK1SachberichtJsonSchema.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Copyright (C) 2025 SYSTOPIA GmbH
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation in version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+namespace Civi\Funding\FundingCaseTypes\AdB\SonstigeAktivitaet\Report\JsonSchema;
+
+use Civi\RemoteTools\JsonSchema\JsonSchema;
+use Civi\RemoteTools\JsonSchema\JsonSchemaDataPointer;
+use Civi\RemoteTools\JsonSchema\JsonSchemaObject;
+use Civi\RemoteTools\JsonSchema\JsonSchemaString;
+use Civi\RemoteTools\JsonSchema\Util\JsonSchemaUtil;
+
+final class AVK1SachberichtJsonSchema extends JsonSchemaObject {
+
+  public function __construct() {
+    parent::__construct([
+      'durchgefuehrt' => new JsonSchemaString([
+        'oneOf' => JsonSchemaUtil::buildTitledOneOf2([
+          'entsprechend dem geplanten Programm' => 'geplant',
+          'mit folgenden wesentlichen Änderungen (kurze Begründung für die Änderung):' => 'geaendert',
+        ]),
+      ]),
+      'aenderungen' => new JsonSchemaString([
+        '$validations' => [
+          JsonSchema::fromArray([
+            'keyword' => 'evaluate',
+            'value' => [
+              'expression' => 'data != "" || durchgefuehrt === "geplant"',
+              'variables' => [
+                'durchgefuehrt' => new JsonSchemaDataPointer('1/durchgefuehrt'),
+              ],
+            ],
+            'message' => 'Bitte Begründung für die Änderungen angeben.',
+          ]),
+        ],
+      ], TRUE),
+      'thematischeSchwerpunkte' => new JsonSchemaString(['minLength' => 1]),
+      'methoden' => new JsonSchemaString(['minLength' => 1]),
+      'zielgruppe' => new JsonSchemaString(['minLength' => 1]),
+      'sonstiges' => new JsonSchemaString(['minLength' => 1]),
+    ], [
+      'required' => [
+        'durchgefuehrt',
+        'thematischeSchwerpunkte',
+        'methoden',
+        'zielgruppe',
+        'sonstiges',
+      ],
+    ]);
+  }
+
+}
