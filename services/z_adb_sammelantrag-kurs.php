a0ffb222ef03faff93509600149dffffed12134d
Dominic Tubach <tubach@systopia.de>
2026-03-27 11:48:16 +0100

    AdB: Register funding case type services
    The Angular module subscribers are removed because we use the forms
    generated from the JSON Forms specs instead of custom AngularJS
    templates.

---
+++ services/z_adb_sammelantrag-kurs.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ * Copyright (C) 2026 SYSTOPIA GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types = 1);
+
+// phpcs:disable Drupal.Commenting.DocComment.ContentAfterOpen
+/** @var \Symfony\Component\DependencyInjection\ContainerBuilder $container */
+
+use Civi\Funding\DependencyInjection\Util\TaskServiceRegistrator;
+use Civi\Funding\FundingCaseType\MetaData\FundingCaseTypeMetaDataInterface;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Application\Actions\KursApplicationActionsDeterminer;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Application\Actions\KursApplicationStatusDeterminer;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Application\Data\KursApplicationFormDataFactory;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Application\Data\KursApplicationFormFilesFactory;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Application\JsonSchema\KursApplicationJsonSchemaFactory;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Application\UiSchema\KursApplicationUiSchemaFactory;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\EventSubscriber\KursApplicationStatusSubscriber;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\FundingCase\Actions\KursCaseActionsDeterminer;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\FundingCase\Data\KursCaseFormDataFactory;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\FundingCase\JsonSchema\KursCaseJsonSchemaFactory;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\FundingCase\UiSchema\KursCaseUiSchemaFactory;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\FundingCase\Validation\KursCaseValidator;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\KursMetaData;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Report\KursReportDataLoader;
+use Civi\Funding\FundingCaseTypes\AdB\SammelantragKurs\Report\KursReportFormFactory;
+use Symfony\Component\DependencyInjection\Reference;
+
+$container->autowire(KursMetaData::class)
+  ->addTag(FundingCaseTypeMetaDataInterface::class);
+
+$container->autowire(KursApplicationActionsDeterminer::class)
+  ->addTag(KursApplicationActionsDeterminer::SERVICE_TAG);
+$container->autowire(KursApplicationStatusDeterminer::class)
+  ->addTag(KursApplicationStatusDeterminer::SERVICE_TAG);
+
+$container->autowire(KursCaseActionsDeterminer::class)
+  ->addTag(KursCaseActionsDeterminer::SERVICE_TAG);
+
+$container->autowire(KursCaseFormDataFactory::class)
+  ->addTag(KursCaseFormDataFactory::SERVICE_TAG);
+$container->autowire(KursCaseUiSchemaFactory::class)
+  ->addTag(KursCaseUiSchemaFactory::SERVICE_TAG);
+$container->autowire(KursCaseJsonSchemaFactory::class)
+  ->addTag(KursCaseJsonSchemaFactory::SERVICE_TAG);
+$container->autowire(KursCaseValidator::class)
+  ->setArgument('$jsonSchemaFactory', new Reference(KursCaseJsonSchemaFactory::class))
+  ->addTag(KursCaseValidator::SERVICE_TAG);
+
+$container->autowire(KursApplicationJsonSchemaFactory::class)
+  ->addTag(KursApplicationJsonSchemaFactory::SERVICE_TAG);
+$container->autowire(KursApplicationUiSchemaFactory::class)
+  ->addTag(KursApplicationUiSchemaFactory::SERVICE_TAG);
+$container->autowire(KursApplicationFormDataFactory::class)
+  ->addTag(KursApplicationFormDataFactory::SERVICE_TAG);
+$container->autowire(KursApplicationFormFilesFactory::class)
+  ->addTag(KursApplicationFormFilesFactory::SERVICE_TAG);
+
+TaskServiceRegistrator::autowireAll(
+  $container,
+  __DIR__ . '/../Civi/Funding/FundingCaseTypes/AdB/SammelantragKurs/Task',
+  'Civi\\Funding\\FundingCaseTypes\\AdB\\SammelantragKurs\\Task'
+);
+
+$container->autowire(KursApplicationStatusSubscriber::class)
+  ->addTag('kernel.event_subscriber')
+  ->setLazy(TRUE);
+
+$container->autowire(KursReportDataLoader::class)
+  ->addTag(KursReportDataLoader::SERVICE_TAG);
+$container->autowire(KursReportFormFactory::class)
+  ->addTag(KursReportFormFactory::SERVICE_TAG);
