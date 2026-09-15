eee890184bda16f34d6cf2d0d2a55a700ca8d24e
Dominic Tubach <tubach@systopia.de>
2026-09-15 09:54:57 +0200

    DVV: Maximum 28 event days

--- Civi/Funding/FundingCaseTypes/DVV/SonstigeAktivitaet/Application/UISchema/AVK1GrunddatenUiSchema.php
+++ Civi/Funding/FundingCaseTypes/DVV/SonstigeAktivitaet/Application/UISchema/AVK1GrunddatenUiSchema.php
@@ -79,6 +79,7 @@ final class AVK1GrunddatenUiSchema extends JsonFormsCategory {
           'removeButtonLabel' => 'Zeitraum entfernen',
         ]
       ),
+      new JsonFormsControl("$scopePrefix/programmtage", 'Veranstaltungstage'),
       new JsonFormsGroup('Teilnehmer*innen', [
         new JsonFormsControl(
           "$scopePrefix/teilnehmer/properties/gesamt",
