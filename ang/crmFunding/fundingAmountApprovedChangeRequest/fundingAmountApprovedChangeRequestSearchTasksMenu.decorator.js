257a21c92aaf12664454c467d95351bd0403a154
Matthias Gutjahr <gutjahr@systopia.de>
2026-09-16 11:34:43 +0200

    Check for allowed actions by change request

--- ang/crmFunding/fundingAmountApprovedChangeRequest/fundingAmountApprovedChangeRequestSearchTasksMenu.decorator.js
+++ ang/crmFunding/fundingAmountApprovedChangeRequest/fundingAmountApprovedChangeRequestSearchTasksMenu.decorator.js
@@ -66,52 +66,103 @@ fundingModule.directive('fundingChangeRequestTasksDecorator', function () {
           taskManager.entityInfo.title = ts('Amount Approved Change Request');
           taskManager.entityInfo.title_plural = ts('Amount Approved Change Requests');
 
+          let allowedActionsByChangeRequest = {};
           const searchKitTasks = taskManager.tasks;
 
           function updateAvailableTasks() {
-            const definedTasks = {
-              approve: {
-                name: 'approve',
-                title: ts('Approve'),
-                confirm: ts('Do you want to approve the selected change requests?'),
-                _isChangeRequestTask: true,
-              },
-              approvePartial: {
-                name: 'approvePartial',
-                title: ts('Approve with change'),
-                confirm: ts('Do you want to approve the selected change requests with changes?'),
-                _isChangeRequestTask: true,
-              },
-              reject: {
-                name: 'reject',
-                title: ts('Reject'),
-                confirm: ts('Do you want to reject the selected change requests?'),
-                _isChangeRequestTask: true,
-              },
-            };
-            taskManager.tasks = searchKitTasks.map(task => {
-              const taskName = task.name || task;
-              if (definedTasks[taskName]) {
-                const custom = definedTasks[taskName];
-                delete definedTasks[taskName];
-                return Object.assign({}, task, custom);
-              }
-              return task;
-            }).concat(Object.values(definedTasks));
-          }
-
-          taskManager.doTask = function (action, ids) {
-            const actionName = typeof action === 'string' ? action : (action.name || action);
-            if (!action._isChangeRequestTask && !['approve', 'approvePartial', 'reject'].includes(actionName)) {
-              return Object.getPrototypeOf(taskManager).doTask.call(taskManager, action, ids);
+            if (ctrl.ids.length === 0) {
+              taskManager.tasks = searchKitTasks;
+              return;
             }
 
-            const name = action._isChangeRequestTask ? action.name : actionName;
-            const confirmMsg = action.confirm || (
-              name === 'approve' ? ts('Do you want to approve the selected change requests?') :
-                name === 'approvePartial' ? ts('Do you want to approve the selected change requests with changes?') :
-                  ts('Do you want to reject the selected change requests?')
-            );
+            let tasks = {};
+            const firstActions = allowedActionsByChangeRequest[ctrl.ids[0]] || {};
+            for (const [actionName, {label, confirm}] of Object.entries(firstActions)) {
+              tasks[actionName] = {
+                name: actionName,
+                title: label,
+                confirm: confirm,
+                _customTask: true,
+              };
+            }
+
+            // Filter out tasks that are not available in all selected change requests
+            // or have a different label.
+            for (let i = 1; i < ctrl.ids.length; ++i) {
+              const actions = allowedActionsByChangeRequest[ctrl.ids[i]] || {};
+              tasks = _4.pickBy(tasks, (task, actionName) => actions[actionName] && actions[actionName].label === task.title);
+            }
+
+            taskManager.tasks = searchKitTasks.concat(Object.values(tasks));
+          }
+
+          let lastIds;
+
+          function updateTasks() {
+            if (_4.isEqual(lastIds, ctrl.ids)) {
+              return new Promise((resolve) => resolve([]));
+            }
+
+            lastIds = _4.clone(ctrl.ids);
+            const idsToGetActions = [];
+            ctrl.ids.forEach((id) => {
+              if (!allowedActionsByChangeRequest[id]) {
+                idsToGetActions.push(id);
+              }
+            });
+
+            if (idsToGetActions.length > 0) {
+              taskManager.tasks = searchKitTasks;
+              return crmApi4('FundingAmountApprovedChangeRequest', 'get', {
+                select: ['id', 'CAN_review'],
+                where: [['id', 'IN', idsToGetActions]]
+              }).then(function (changeRequests) {
+                for (const req of changeRequests) {
+                  if (req.CAN_review) {
+                    allowedActionsByChangeRequest[req.id] = {
+                      approve: {
+                        label: ts('Approve'),
+                        confirm: ts('Do you want to approve the selected change requests?'),
+                      },
+                      approvePartial: {
+                        label: ts('Approve with change'),
+                        confirm: ts('Do you want to approve the selected change requests with changes?'),
+                      },
+                      reject: {
+                        label: ts('Reject'),
+                        confirm: ts('Do you want to reject the selected change requests?'),
+                      },
+                    };
+                  }
+                  else {
+                    allowedActionsByChangeRequest[req.id] = {};
+                  }
+                }
+
+                updateAvailableTasks();
+              });
+            }
+            else {
+              updateAvailableTasks();
+              return new Promise((resolve) => resolve([]));
+            }
+          }
+
+          // Only triggered if a new ID is selected, not on deselect.
+          $scope.$watch('$ctrl.ids', () => updateTasks());
+
+          taskManager.getMetadata = updateTasks;
+
+          const parentDoTask = taskManager.doTask;
+          taskManager.doTask = function (action, ids) {
+            if (!action._customTask) {
+              parentDoTask(action, ids);
+
+              return;
+            }
+
+            const actionName = action.name;
+            const confirmMsg = action.confirm;
 
             const run = function (targetIds) {
               if (targetIds.length === 0) {
@@ -119,7 +170,7 @@ fundingModule.directive('fundingChangeRequestTasksDecorator', function () {
                 return;
               }
 
-              if (name === 'approvePartial') {
+              if (actionName === 'approvePartial') {
                 CRM.confirm({
                   title: ts('Approve with change'),
                   width: '400px',
@@ -127,7 +178,7 @@ fundingModule.directive('fundingChangeRequestTasksDecorator', function () {
                   options: { no: ts('Cancel'), yes: ts('Confirm') },
                 }).on('crmConfirm:yes', function () {
                   const amount = document.getElementById('change-request-approved-amount').value;
-                  executeTask(name, targetIds, {
+                  executeTask(actionName, targetIds, {
                     amountApproved: amount,
                   });
                 });
@@ -136,12 +187,12 @@ fundingModule.directive('fundingChangeRequestTasksDecorator', function () {
                   approve: ts('Approve'),
                   reject: ts('Reject'),
                 };
-                const yesLabel = yesLabels[name] || ts('Continue');
+                const yesLabel = yesLabels[actionName] || ts('Continue');
                 CRM.confirm({
                   message: confirmMsg,
                   options: { no: ts('Cancel'), yes: yesLabel },
                 }).on('crmConfirm:yes', function () {
-                  executeTask(name, targetIds, {});
+                  executeTask(actionName, targetIds, {});
                 });
               }
             };
@@ -160,19 +211,30 @@ fundingModule.directive('fundingChangeRequestTasksDecorator', function () {
           function executeTask(actionName, ids, params) {
             const apiParams = Object.assign({ ids: ids }, params);
             crmStatus({}, crmApi4('FundingAmountApprovedChangeRequest', actionName, apiParams).then(() => {
+              for (const id of ids) {
+                allowedActionsByChangeRequest[id] = {};
+              }
               taskManager.refreshAfterTask();
+
+              const event = new CustomEvent('fundingAmountApprovedChangeRequestSearchTaskExecuted', {
+                detail: {
+                  entity: entityName,
+                  ids: ids,
+                  action: actionName,
+                },
+              });
+              document.dispatchEvent(event);
             }));
           }
 
           const parentIsActionAllowed = ctrl.isActionAllowed;
           ctrl.isActionAllowed = function (action) {
-            if (action._isChangeRequestTask || ['approve', 'approvePartial', 'reject'].includes(action.name || action)) {
+            if (action._customTask) {
               return true;
             }
+
             return parentIsActionAllowed(action);
           };
-
-          updateAvailableTasks();
         });
       },
     ],
