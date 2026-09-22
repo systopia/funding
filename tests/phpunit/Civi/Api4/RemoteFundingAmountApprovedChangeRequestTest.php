cd68b69b1662983564007453adc7c5d338c12c6f
Matthias Gutjahr <gutjahr@systopia.de>
2026-09-22 08:52:53 +0200

    Add additional test cases for RemoteFundingAmountApprovedChangeRequest

--- tests/phpunit/Civi/Api4/RemoteFundingAmountApprovedChangeRequestTest.php
+++ tests/phpunit/Civi/Api4/RemoteFundingAmountApprovedChangeRequestTest.php
@@ -4,17 +4,16 @@ declare(strict_types = 1);
 
 namespace Civi\Api4;
 
+use Civi\API\Exception\UnauthorizedException;
 use Civi\Funding\AbstractRemoteFundingHeadlessTestCase;
 use Civi\Funding\Entity\FundingCaseEntity;
-use Civi\Funding\FileTypeNames;
-use Civi\Funding\Fixtures\AttachmentFixture;
 use Civi\Funding\Fixtures\ContactFixture;
+use Civi\Funding\Fixtures\FundingAmountApprovedChangeRequestFixture;
 use Civi\Funding\Fixtures\FundingCaseContactRelationFixture;
 use Civi\Funding\Fixtures\FundingCaseFixture;
 use Civi\Funding\Fixtures\FundingCaseTypeFixture;
 use Civi\Funding\Fixtures\FundingProgramContactRelationFixture;
 use Civi\Funding\Fixtures\FundingProgramFixture;
-use CRM_Funding_ExtensionUtil as E;
 
 /**
  * @covers \Civi\Api4\RemoteFundingAmountApprovedChangeRequest
@@ -26,35 +25,23 @@ final class RemoteFundingAmountApprovedChangeRequestTest extends AbstractRemoteF
 
   public function testCreate(): void {
     $fundingCase = $this->createFundingCase();
-    AttachmentFixture::addFixture(
-      'civicrm_funding_case',
-      $fundingCase->getId(),
-      E::path('tests/phpunit/resources/FundingCaseDocumentTemplate.docx'),
-      ['file_type_id:name' => FileTypeNames::TRANSFER_CONTRACT],
-    );
 
     $contact = ContactFixture::addIndividual();
     FundingProgramContactRelationFixture::addContact(
       $contact['id'],
       $fundingCase->getFundingProgramId(),
-      [
-        'application_program_perm',
-        'CAN_create_amount_approved_change_request',
-        'amountApprovedChangeRequest_create',
-      ]
+      []
     );
-    FundingCaseContactRelationFixture::addContact($contact['id'], $fundingCase->getId(), [
-      'application_case_perm',
-      'CAN_create_amount_approved_change_request',
-      'amountApprovedChangeRequest_create',
-    ]);
+    FundingCaseContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getId(),
+      ['amountApprovedChangeRequest_create']
+    );
 
-    $request = RemoteFundingAmountApprovedChangeRequest::create()
-      ->setRemoteContactId((string) $contact['id'])
-      ->setFundingCaseId($fundingCase->getId())
-      ->setAmountRequested(200.0)
-      ->setComment('Test comment');
-    $request->execute();
+    FundingAmountApprovedChangeRequestFixture::addFixture(
+      $fundingCase->getId(),
+      $contact['id']
+    );
 
     $result = RemoteFundingAmountApprovedChangeRequest::create()
       ->setRemoteContactId((string) $contact['id'])
@@ -67,6 +54,130 @@ final class RemoteFundingAmountApprovedChangeRequestTest extends AbstractRemoteF
     static::assertSame($fundingCase->getId(), $record['funding_case_id']);
     static::assertSame(200.0, $record['amount_requested']);
     static::assertSame('Test comment', $record['comment']);
+  }
+
+  public function testCreateNotAllowedWithoutAmountApprovedChangeRequestCreatePermission(): void {
+    $fundingCase = $this->createFundingCase();
+
+    $contact = ContactFixture::addIndividual();
+    FundingProgramContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getFundingProgramId(),
+      []
+    );
+    FundingCaseContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getId(),
+      ['application_case_perm']
+    );
+
+    FundingAmountApprovedChangeRequestFixture::addFixture(
+      $fundingCase->getId(),
+      $contact['id']
+    );
+
+    $this->expectException(UnauthorizedException::class);
+    $this->expectExceptionMessage('Not authorized to create this change request.');
+
+    RemoteFundingAmountApprovedChangeRequest::create()
+      ->setRemoteContactId((string) $contact['id'])
+      ->setFundingCaseId($fundingCase->getId())
+      ->setAmountRequested(200.0)
+      ->setComment('Test comment')
+      ->execute();
+  }
+
+  public function testGet(): void {
+    $fundingCase = $this->createFundingCase();
+
+    $contact = ContactFixture::addIndividual();
+    FundingProgramContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getFundingProgramId(),
+      []
+    );
+    FundingCaseContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getId(),
+      ['amountApprovedChangeRequest_create']
+    );
+
+    $request = FundingAmountApprovedChangeRequestFixture::addFixture(
+      $fundingCase->getId(),
+      $contact['id'],
+      ['amount_requested' => 123.45]
+    );
+
+    $result = RemoteFundingAmountApprovedChangeRequest::get()
+      ->setRemoteContactId((string) $contact['id'])
+      ->addSelect('id', 'funding_case_id', 'amount_requested')
+      ->execute();
+
+    static::assertCount(1, $result);
+    static::assertSame(
+      [
+        'id' => $request->getId(),
+        'funding_case_id' => $fundingCase->getId(),
+        'amount_requested' => 123.45,
+      ],
+      $result->first()
+    );
+  }
+
+  public function testGetNotAllowedWithoutAmountApprovedChangeRequestCreatePermission(): void {
+    $fundingCase = $this->createFundingCase();
+
+    $contact = ContactFixture::addIndividual();
+    FundingProgramContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getFundingProgramId(),
+      []
+    );
+    FundingCaseContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getId(),
+      ['application_case_perm']
+    );
+
+    FundingAmountApprovedChangeRequestFixture::addFixture(
+      $fundingCase->getId(),
+      $contact['id']
+    );
+
+    $result = RemoteFundingAmountApprovedChangeRequest::get()
+      ->setRemoteContactId((string) $contact['id'])
+      ->addSelect('id')
+      ->execute();
+
+    static::assertCount(1, $result);
+  }
+
+  public function testGetNotAllowedWithoutFundingCaseAccess(): void {
+    $fundingCase = $this->createFundingCase();
+
+    $contact = ContactFixture::addIndividual();
+    FundingProgramContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getFundingProgramId(),
+      []
+    );
+    FundingCaseContactRelationFixture::addContact(
+      $contact['id'],
+      $fundingCase->getId(),
+      []
+    );
+
+    FundingAmountApprovedChangeRequestFixture::addFixture(
+      $fundingCase->getId(),
+      $contact['id']
+    );
+
+    $result = RemoteFundingAmountApprovedChangeRequest::get()
+      ->setRemoteContactId((string) $contact['id'])
+      ->addSelect('id')
+      ->execute();
+
+    static::assertCount(0, $result);
   }
 
   private function createFundingCase(): FundingCaseEntity {
